18c204776137bffadf717608785a23f046bba38a
Luis Thiam-Nye <luis.tn@protonmail.ch>
2022-07-06 00:14:39 +0100

    finalised design concept for linear tree view

--- src/chic/clj_editor2/file_tree.clj
+++ src/chic/clj_editor2/file_tree.clj
@@ -38,7 +38,7 @@
 (defn a-view []
   (let [font (Font. style/face-default (float 14))]
     (ui2/stack
-     (ui2/fill-rect (huipaint/fill (cpaint/grey 245)))
+     (ui2/fill-rect (huipaint/fill (unchecked-int 0xFFeff2f7)#_(cpaint/grey 245)))
      (ui2/column*
       (mapv (fn [[f n p]]
               (ui2/sized-with
@@ -48,69 +48,146 @@
                 ((ui2/direct-widget
                   {:draw
                    (fn [w {:keys [scale]} ^Rect rect ^Canvas cnv]
-                     (let [idiff 16.
+                     (let [idiff 17.
                            fil? (== p (dec n))
                            gl (+ 16. (* n idiff))
                            gw 2.
                            vw 10.
                            ;; ic (cpaint/grey 225)
-                           ic (unchecked-int 0xFFc9dcf1);(cpaint/grey 220)
-                           buc 0xdF1f5488
+                           ic (unchecked-int 0xFFc9dcf1) ;(cpaint/grey 220)
+                           ;; buc 0xdF1f5488
+                           ;; buc (unchecked-int 0xFF136ab5)
+                           ;; buc (unchecked-int 0xFF8050b1)
+                           ;; buc (unchecked-int 0xFF9062c5)
+                           ;; buc (unchecked-int 0xFF9b6fcf)
+                           buc (unchecked-int 0xFFb37bfa)
                            gpaint (doto (Paint.)
                                     (.setShader
                                      (Shader/makeLinearGradient
                                       gl 0. (+ gl gw) 0.
                                       (int-array [ic (cpaint/grey 245)]))))]
+                       #_(dotimes [r (max 0 (dec n))]
+                           (.drawRect cnv
+                                      (Rect/makeXYWH
+                                       (+ -2 (* (+ 2 r) idiff) (:x rect))
+                                       (+ -3 (:bottom rect))
+                                       2.
+                                       4.)
+                                      (huipaint/fill
+                                       (- buc (* (- n r) 0x05000000)
+                                          0xca000000))))
+                       (dotimes [r n]
+                         (let [left (+ -6 (* (+ 2 r) idiff) (:x rect))
+                               right (+ 2 (* (+ 2 r) idiff) (:x rect))
+                               ;; col (unchecked-int 0x104c9fe5)
+                               col (unchecked-int 0xFFf3e4ff)
+                               ;; col (unchecked-int 0xFFffffff)
+                               col' (- col (* (- n r) 0x02000000))]
+                           (.drawRect cnv
+                                      (Rect.
+                                       left (+ (:y rect))
+                                       right (+ (:bottom rect)))
+                                      (doto (Paint.)
+                                        (.setShader
+                                         (Shader/makeLinearGradient
+                                          left 0. right 0.
+                                          (int-array [(unchecked-int 0) col' (unchecked-int 0)])
+                                          (float-array [0. 0.7 1.])))))
+                           #_(.drawRect cnv
+                                        (Rect.
+                                         (+ -2 (* (+ 2 r) idiff) (:x rect))
+                                         (+ (:y rect))
+                                         (+ -1 (* (+ 2 r) idiff) (:x rect))
+                                         (+ (:bottom rect)))
+                                        (huipaint/fill col'))
+                           #_(let [left (+ -1 (* (+ 2 r) idiff) (:x rect))
+                                   right (+ 2 (* (+ 2 r) idiff) (:x rect))]
+                               (.drawRect cnv
+                                          (Rect.
+                                           left (+ (:y rect))
+                                           right (+ (:bottom rect)))
+                                          (doto (Paint.)
+                                            (.setShader
+                                             (Shader/makeLinearGradient
+                                              left 0. right 0.
+                                              (int-array [col' (unchecked-int 0)]))))))))
+                       #_(dotimes [r n]
+                           (let [l (+ -6 (* (+ 2 r) idiff) (:x rect))
+                                 right (+ -2 (* (+ 2 r) idiff) (:x rect))]
+                             (.drawRect cnv
+                                        (Rect.
+                                         l (+ 1 (:y rect))
+                                         right (+ -3 (:bottom rect)))
+                                        (doto (Paint.)
+                                          (.setShader
+                                           (Shader/makeLinearGradient
+                                            l 0. right 0.
+                                            (int-array [(unchecked-int 0)
+                                                        (- buc (* (- n r) 0x02000000)
+                                                           0xe9000000)]))))))
+                           (.drawRect cnv
+                                      (Rect.
+                                       (+ -2 (* (+ 2 r) idiff) (:x rect))
+                                       (+ 1 (:y rect))
+                                       (+ (* (+ 2 r) idiff) (:x rect))
+                                       (+ -3 (:bottom rect)))
+                                      (huipaint/fill
+                                       (- buc (* (- n r) 0x02000000)
+                                          0xe9000000))))
                        (when (< 0 n)
                          #_(.drawRect cnv (Rect. (+ (:x rect) (+ 16. (* n 16.))
-                                                  (- 0 gw vw)) (:y rect)
-                                               (+ (:x rect) (+ 16. (* n 16.))
-                                                  (- vw))
-                                               (:bottom rect))
-                                    ;; (huipaint/fill ic)
-                                    (doto (Paint.)
-                                      (.setShader
-                                       (Shader/makeLinearGradient
-                                        (- (+ (:x rect) (+ 16. (* n 16.))) gw vw)
-                                        0. (+ (:x rect) (+ 16. (* n 16.)) (- vw)) 0.
-                                        (int-array [(unchecked-int 0) ic])))))
+                                                    (- 0 gw vw)) (:y rect)
+                                                 (+ (:x rect) (+ 16. (* n 16.))
+                                                    (- vw))
+                                                 (:bottom rect))
+                                      ;; (huipaint/fill ic)
+                                      (doto (Paint.)
+                                        (.setShader
+                                         (Shader/makeLinearGradient
+                                          (- (+ (:x rect) (+ 16. (* n 16.))) gw vw)
+                                          0. (+ (:x rect) (+ 16. (* n 16.)) (- vw)) 0.
+                                          (int-array [(unchecked-int 0) ic])))))
                          (.drawRect cnv (Rect. (:x rect) (:y rect)
-                                               (+ (:x rect) (+ 16. (* n 16.))
+                                               (+ (:x rect) (+ 16. (* n idiff))
                                                   (- vw))
                                                (:bottom rect))
                                     (huipaint/fill (- ic 0xda000000)))
                          #_(.drawRect cnv (Rect. (+ (:x rect) (+ 16. (* n 16.))
-                                                  (- vw)) (:y rect)
-                                               (+ (:x rect) (+ 16. (* n 16.)))
-                                               (:bottom rect))
-                                    (huipaint/fill ic))
-                         (.drawRect cnv (Rect. (+ (:x rect) (+ 16. (* n 16.))
-                                                  (- vw)) (:y rect)
-                                               (+ (:x rect) (+ 16. (* n 16.)))
-                                               (+ (:y rect) 6.))
-                                    (huipaint/fill ic))
+                                                    (- vw)) (:y rect)
+                                                 (+ (:x rect) (+ 16. (* n 16.)))
+                                                 (:bottom rect))
+                                      (huipaint/fill ic))
+                         #_#_(.drawRect cnv (Rect. (+ (:x rect) (+ 16. (* n 16.))
+                                                      (- vw)) (:y rect)
+                                                   (+ (:x rect) (+ 16. (* n 16.)))
+                                                   (+ (:y rect) 6.))
+                                        (huipaint/fill ic))
                          (.drawRect cnv (Rect. (+ (:x rect) (+ 16. (* n 16.))
                                                   (- vw)) (- (:bottom rect) 6.)
                                                (+ (:x rect) (+ 16. (* n 16.)))
                                                (:bottom rect))
                                     (huipaint/fill ic))
                          (when fil?
-                           (.drawRect cnv (Rect. (+ (:x rect) (+ 16. (* n 16.))
-                                                    (- 2.)) (:y rect)
-                                                 (+  (:x rect) (+ 16. (* n 16.)))
-                                                (+ (:y rect) 2.))
-                                     (huipaint/fill buc)))
-                         (.drawRect cnv (Rect. (+ (:x rect) (+ 16. (* n 16.))
+                           (.drawRect cnv (Rect. (+ 1. (:x rect) (+ 16. (* n idiff))
+                                                    (- 2.))
+                                                 (+ -1 (:y rect))
+                                                 (+ 1. (:x rect) (+ 16. (* n idiff)))
+                                                 (+ (:y rect) 2.))
+                                      (huipaint/fill buc)))
+                         (.drawRect cnv (Rect. (+ (:x rect) (+ 16. (* n idiff))
                                                   (- 1)) (- (:bottom rect) 3.)
-                                               (+ 1 (:x rect) (+ 16. (* n 16.)))
-                                               (:bottom rect))
+                                               (+ 1 (:x rect) (+ 16. (* n idiff)))
+                                               (+ -1 (:bottom rect)))
                                     (huipaint/fill buc)))
                        #_(.drawRect cnv (.withWidth (.offset rect (Point. gl 0.))
-                                                  gw)
-                                  gpaint)
-                       (.drawRect cnv (Rect. (:x rect) (- (:bottom rect) 1)
-                                             (+ (:x rect) gl) (:bottom rect))
-                                  (huipaint/fill (cpaint/grey 200)))
+                                                    gw)
+                                    gpaint)
+                       #_(.drawRect cnv (Rect. (:x rect) (- (:bottom rect) 1)
+                                               (+ (:x rect) gl) (:bottom rect))
+                                    (huipaint/fill (cpaint/grey 200)))
+                       (.drawRect cnv (Rect. (+ (:x rect) gl) (:bottom rect)
+                                             (+ (:x rect) gl idiff) (+ 1 (:bottom rect)))
+                                  (huipaint/fill (- buc 0x90000000)))
                        (.drawRect cnv (Rect. (+ (:x rect) gl) (- (:bottom rect) 2)
                                              (+ (:x rect) gl idiff) (:bottom rect))
                                   (huipaint/fill #_(cpaint/grey 120)
@@ -123,7 +200,8 @@
                   (ui2/svg-from-data (maticons/svg-data "folder" "outlined" "24px"))))
                 (ui2/padded
                  [20. 1. 6. (+ 3 (Math/ceil (.getDescent (.getMetrics font))))]
-                 (ui2/textline (uifont/shape-line-default font (fs/file-name f)))))))
+                 (ui2/textline (uifont/shape-line-default font (fs/file-name f))
+                               (huipaint/fill 0xEc000000))))))
             (map vector
                  (fs/list-dir (io/file "."))
                  [0 0 0 1 1 2 3 4 4 1 2 2 2 1]
